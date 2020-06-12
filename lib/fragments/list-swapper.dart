e6785d479864ef35fc77263df24bdc40a7ed803f
BugTheDebugger <bprayush@yahoo.com>
2020-06-12 12:14:25 +0545

    Refactoring code for readability

--- lib/fragments/list-swapper.dart
+++ lib/fragments/list-swapper.dart
@@ -53,7 +53,7 @@ class ListSwapper extends StatelessWidget {
                     )
                     .toList(),
               ),
-            )
+            ),
           ],
         ),
         Expanded(
